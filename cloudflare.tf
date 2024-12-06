edf21216b5fcd1360051b630401529fcd1f7a75d
Danielle Maywood <danielle@themaywoods.com>
2024-12-06 12:12:58 +0000

    update cloudflare pages

--- cloudflare.tf
+++ cloudflare.tf
@@ -12,7 +12,7 @@ resource "cloudflare_zone" "daniellemaywood_uk" {
 resource "cloudflare_record" "daniellemaywood_uk_CNAME_pages" {
   zone_id = cloudflare_zone.daniellemaywood_uk.id
   name    = "daniellemaywood.uk"
-  value   = cloudflare_pages_project.home.subdomain
+  content = cloudflare_pages_project.home.subdomain
   type    = "CNAME"
   proxied = true
 }
@@ -20,14 +20,14 @@ resource "cloudflare_record" "daniellemaywood_uk_CNAME_pages" {
 resource "cloudflare_record" "daniellemaywood_uk_TXT_google" {
   zone_id = cloudflare_zone.daniellemaywood_uk.id
   name    = "daniellemaywood.uk"
-  value   = "google-site-verification=VDJzEVRpRsxmJv0OXBIsvJTp0Xa1PwdYihMH47VDx7A"
+  content = "google-site-verification=VDJzEVRpRsxmJv0OXBIsvJTp0Xa1PwdYihMH47VDx7A"
   type    = "TXT"
 }
 
 resource "cloudflare_record" "_atproto_daniellemaywood_uk_TXT_bsky" {
   zone_id = cloudflare_zone.daniellemaywood_uk.id
   name    = "_atproto"
-  value   = "did=did:plc:b4wthggvovz2uwoet47bstfp"
+  content = "did=did:plc:b4wthggvovz2uwoet47bstfp"
   type    = "TXT"
 }
 
@@ -41,6 +41,26 @@ resource "cloudflare_pages_project" "home" {
   account_id        = var.cf_account_id
   name              = "daniellemaywood"
   production_branch = "prod"
+
+  build_config {
+    build_command   = "npm run build"
+    destination_dir = "dist"
+  }
+
+  source {
+    type = "github"
+    config {
+      owner     = "DanielleMaywood"
+      repo_name = "daniellemaywood.uk"
+
+      production_branch             = "main"
+      production_deployment_enabled = true
+    }
+  }
+
+  deployment_configs {
+    production {}
+  }
 }
 
 resource "cloudflare_pages_domain" "daniellemaywood_uk" {
