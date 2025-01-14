11305e72a9b467ba154ec4466689fadf9a68a13f
Danielle Maywood <danielle@themaywoods.com>
2025-01-14 18:41:41 +0000

    add tea.ht

--- cloudflare.tf
+++ cloudflare.tf
@@ -1,5 +1,32 @@
 /*************************************************
 *                                                *
+*            ZONE: tea.hut                       *
+*                                                *
+*************************************************/
+
+resource "cloudflare_zone" "tea_ht" {
+  account_id = var.cf_account_id
+  zone       = "tea.ht"
+}
+
+resource "cloudflare_record" "tea_ht_A" {
+  zone_id = cloudflare_zone.tea_ht.id
+  name    = "tea.ht"
+  content = var.winnie.a
+  type    = "A"
+  proxied = false
+}
+
+resource "cloudflare_record" "tea_ht_AAAA" {
+  zone_id = cloudflare_zone.tea_ht.id
+  name    = "tea.ht"
+  content = var.winnie.aaaa
+  type    = "AAAA"
+  proxied = false
+}
+
+/*************************************************
+*                                                *
 *            ZONE: daniellemaywood.uk            *
 *                                                *
 *************************************************/
@@ -7,6 +34,16 @@
 resource "cloudflare_zone" "daniellemaywood_uk" {
   account_id = var.cf_account_id
   zone       = "daniellemaywood.uk"
+}
+
+resource "cloudflare_zone_dnssec" "daniellemaywood_uk" {
+  zone_id = cloudflare_zone.daniellemaywood_uk.id
+}
+
+resource "cloudflare_bot_management" "daniellemaywood_uk" {
+  zone_id            = cloudflare_zone.daniellemaywood_uk.id
+  ai_bots_protection = "block"
+  fight_mode         = false
 }
 
 resource "cloudflare_record" "daniellemaywood_uk_CNAME_pages" {
@@ -29,6 +66,38 @@ resource "cloudflare_record" "_atproto_daniellemaywood_uk_TXT_bsky" {
   name    = "_atproto"
   content = "did=did:plc:b4wthggvovz2uwoet47bstfp"
   type    = "TXT"
+}
+
+resource "cloudflare_record" "daniellemaywood_uk_A_chef" {
+  zone_id = cloudflare_zone.daniellemaywood_uk.id
+  name    = "chef"
+  content = var.winnie.a
+  type    = "A"
+  proxied = false
+}
+
+resource "cloudflare_record" "daniellemaywood_uk_AAAA_chef" {
+  zone_id = cloudflare_zone.daniellemaywood_uk.id
+  name    = "chef"
+  content = var.winnie.aaaa
+  type    = "AAAA"
+  proxied = false
+}
+
+resource "cloudflare_record" "daniellemaywood_uk_A_coder" {
+  zone_id = cloudflare_zone.daniellemaywood_uk.id
+  name    = "coder"
+  content = var.winnie.a
+  type    = "A"
+  proxied = false
+}
+
+resource "cloudflare_record" "daniellemaywood_uk_AAAA_coder" {
+  zone_id = cloudflare_zone.daniellemaywood_uk.id
+  name    = "coder"
+  content = var.winnie.aaaa
+  type    = "AAAA"
+  proxied = false
 }
 
 /*************************************************
--- variables.tf
+++ variables.tf
@@ -1,3 +1,10 @@
 variable "cf_account_id" {
   default = "6b1f203f12648e063d24e9d81346523c"
 }
+
+variable "winnie" {
+  default = {
+    a    = "95.179.238.39"
+    aaaa = "2001:19f0:7402:1f50:5400:5ff:fe3e:4586"
+  }
+}
